10f3a55ff3e6aae61eb35dd47fc5f759d877537e
AlenkinAndrew <oxqq@ya.ru>
2023-06-03 00:37:56 +0300

    codestyle

--- protobuf-consumer/src/main/java/com/afalenkin/protobufconsumer/ProtobufConsumerApplication.java
+++ protobuf-consumer/src/main/java/com/afalenkin/protobufconsumer/ProtobufConsumerApplication.java
@@ -2,8 +2,6 @@ package com.afalenkin.protobufconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
 
 @SpringBootApplication
 public class ProtobufConsumerApplication {
